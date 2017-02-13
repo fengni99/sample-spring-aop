17debd3b4e0027af89fd380505b5ae272261a435
dongliang.ma <dongliang.ma@baozun.cn>
2017-02-13 15:05:01 +0800

    aop study

---
+++ src/main/java/com/discovery/darchrow/TestAop2.java
@@ -0,0 +1,38 @@
+/**
+ * Copyright (c) 2016 Baozun All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information of Baozun.
+ * You shall not disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Baozun.
+ *
+ * BAOZUN MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT THE SUITABILITY OF THE
+ * SOFTWARE, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+ * PURPOSE, OR NON-INFRINGEMENT. BAOZUN SHALL NOT BE LIABLE FOR ANY DAMAGES
+ * SUFFERED BY LICENSEE AS A RESULT OF USING, MODIFYING OR DISTRIBUTING
+ * THIS SOFTWARE OR ITS DERIVATIVES.
+ *
+ */
+package com.discovery.darchrow;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.discovery.darchrow.service.HelloWorld;
+
+public class TestAop2 {
+	
+	@SuppressWarnings("resource")
+	public static void main(String[] args) {
+		ApplicationContext ctx = 
+	            new ClassPathXmlApplicationContext("spring-aop2.xml");
+		HelloWorld hwA =(HelloWorld)ctx.getBean("helloWorldAImpl");
+		HelloWorld hwB =(HelloWorld)ctx.getBean("helloWorldBImpl");
+		hwA.printHello();
+		hwA.doPrint();
+		System.out.println("-----------");
+		hwB.printHello();
+		hwB.doPrint();
+	}
+}
